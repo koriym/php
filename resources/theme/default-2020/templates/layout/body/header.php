a6dbfee4fb67af9307d7a028c03ca77e5f049493
Paul M. Jones <pmjones88@gmail.com>
2020-11-10 16:35:03 -0600

    wip

--- resources/theme/default-2020/templates/layout/body/header.php
+++ resources/theme/default-2020/templates/layout/body/header.php
@@ -1,12 +1,8 @@
-<header class="pt-4 pb-0 text-center">
+<header class="text-center">
     <?= $this->render('templates', $this->config->theme->layout_header_prepend ?? []) ?>
     <h1><?= $this->config->general->title; ?></h1>
     <p class=><?= $this->config->general->tagline; ?></p>
     <?= $this->render('templates', $this->config->theme->layout_header_append ?? []) ?>
-
-    <nav class="navbar pb-0 navbar-expand-md">
-        <ul id="navbar-menu" class="navbar-nav mx-auto text-center">
-            <script async>Argo.shtml('/menu.shtml', 'navbar-menu')</script>
-        </ul>
-    </nav>
 </header>
+
+<?= $this->render('layout/body/nav'); ?>
